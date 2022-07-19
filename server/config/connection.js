84c4a8ff1a4cf3f1d310f5133123ecfd37cae3c7
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-07-18 21:34:58 -0400

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,7 +7,7 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   }
 );
 
